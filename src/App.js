16d526564c7215c32c516c4ce4eb3c3fafbf7008
mk-nazemi <nazemi.works@gmail.com>
2020-09-25 18:51:33 +0330

    first on feature branch

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 222
+          Learn React
         </a>
       </header>
     </div>
